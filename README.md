639b17844480e8c8621404f791258a3062ef2a8c
Victor Lobato <43821236+AquaStarOP@users.noreply.github.com>
2021-12-08 14:41:06 +0000

    Added trail and gravity world control

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # NBody-Simulation
+This is an nbody physics simulator that outputs to a video file
+
+<img src="images/1.png"
+     alt="A line of circles touching end to end increasing in constant size"
+     style="float: left; margin-right: 10px;" />
+
+This can handle multiple video streams and physics simulations simulataneously
+
+<img src="images/2.png"
+     alt="4 circles going counter-clockwise with line trails behind them"
+     style="float: left; margin-right: 10px;" />
+
+Trails can be enabled in the world options
--- main.py
+++ main.py
@@ -18,30 +18,42 @@ class Video:
 
 # Object object
 class Obj:
-  def __init__(self, x, y, m, xv, yv):
+  def __init__(self, x, y, m, xv, yv, trail):
     self.x, self.y = x, y     # Position
     self.m = m                # Mass
     self.xv, self.yv = xv, yv # Velocity
+    self.ps = []              # Positions
+    self.trail = trail        # Trail duration
 
   # Update - built in for simplicity
   def upd(self):
+    self.ps.append((self.x, self.y))
     self.x += self.xv
     self.y += self.yv
+    if len(self.ps) > self.trail:
+      self.ps.pop(0)
 
 # World Object
 class World:
-  def __init__(self):
-    self.objs = [] # Objects
+  def __init__(self, gravity=1, trail=0):
+    self.objs = []          # Objects
+    self.trail = trail      # Trail duration
+    self.gravity = gravity  # Gravity multiplier
 
   # Add an object - velocity optionally specified
   def add(self, x, y, m, xv=0, yv=0):
-    self.objs.append(Obj(x, y, m, xv, yv))
+    self.objs.append(Obj(x, y, m, xv, yv, self.trail))
 
   # Draw the image
   def draw(self, width, height):
     img = Image.new('RGB', (width, height), color='white')
     draw = ImageDraw.Draw(img) # Editable map
     for o in self.objs:
+      for p in range(len(o.ps)-1):
+        px1, py1 = o.ps[p]
+        px2, py2 = o.ps[p+1]
+        draw.line((px1+width/2,py1+height/2,px2+width/2,py2+height/2), fill='black', width=1)
+      # Draw actual object
       x, y = round(o.x)+(width/2), round(o.y)+(height/2)
       if (0<=x<width) and (0<=y<height):
         r = m.sqrt(o.m)
@@ -53,7 +65,7 @@ class World:
     for ac,a in enumerate(self.objs):
       for bc,b in enumerate(self.objs):
         if bc>ac: # Used to avoid double updates
-          f = (a.m*b.m)/((abs(a.x-b.x)**2)+(abs(a.y-b.y)**2)) # Overall force
+          f = (self.gravity*a.m*b.m)/((abs(a.x-b.x)**2)+(abs(a.y-b.y)**2)) # Overall force
           xs = a.x-b.x  # X scale
           ys = a.y-b.y  # Y scale
           ts = abs(xs)+abs(ys) # Total Scale
@@ -68,16 +80,17 @@ class World:
       o.upd()
 
 def main():
-  w = World()
+  w = World(trail=30)
   v = Video(1920, 1080, 60)
-  for i in range(100):
-    w.add(randint(-500,500),randint(-500,500),randint(10,100))
+  for i in range(50):
+     w.add(randint(-200,200),randint(-200,200),randint(10,100))
 
   try:
     for i in range(900):
       w.upd()
       v.write(array(w.draw(v.width, v.height)))
       print(i)
+      w.gravity = 1+(i/900)
   except KeyboardInterrupt:
     pass
   except:
